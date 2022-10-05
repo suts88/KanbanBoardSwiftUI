9046ad2fca4f436f649c0493d2ce28cbc0f0c081
Stefan Utschig <Stefan.Utschig@gmx.de>
2022-10-05 17:09:41 +0200

    Initial Commit

---
+++ KanbanBoard/KanbanBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanbanBoardApp.swift
+//  KanbanBoard
+//
+//  Created by Stefan Utschig on 05.10.22.
+//
+
+import SwiftUI
+
+@main
+struct KanbanBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
